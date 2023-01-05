8ce2f49b24631d1e6f2339ec42196d5157c1bcc8
Joseph Szobody <joseph@stechstudio.com>
2023-01-04 22:57:44 -0500

    big performance improvement. set visibility flags on existing data structure.

--- dist/default.php
+++ dist/default.php
@@ -13,7 +13,7 @@
     <title>Pretty PHP info</title>
     <link rel="shortcut icon" type="image/svg" href="data:image/svg+xml,%0A%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 -1 100 50'%3E%3Cpath d='m7.579 10.123 14.204 0c4.169 0.035 7.19 1.237 9.063 3.604 1.873 2.367 2.491 5.6 1.855 9.699-0.247 1.873-0.795 3.71-1.643 5.512-0.813 1.802-1.943 3.427-3.392 4.876-1.767 1.837-3.657 3.003-5.671 3.498-2.014 0.495-4.099 0.742-6.254 0.742l-6.36 0-2.014 10.07-7.367 0 7.579-38.001 0 0m6.201 6.042-3.18 15.9c0.212 0.035 0.424 0.053 0.636 0.053 0.247 0 0.495 0 0.742 0 3.392 0.035 6.219-0.3 8.48-1.007 2.261-0.742 3.781-3.321 4.558-7.738 0.636-3.71 0-5.848-1.908-6.413-1.873-0.565-4.222-0.83-7.049-0.795-0.424 0.035-0.83 0.053-1.219 0.053-0.353 0-0.724 0-1.113 0l0.053-0.053'/%3E%3Cpath d='m41.093 0 7.314 0-2.067 10.123 6.572 0c3.604 0.071 6.289 0.813 8.056 2.226 1.802 1.413 2.332 4.099 1.59 8.056l-3.551 17.649-7.42 0 3.392-16.854c0.353-1.767 0.247-3.021-0.318-3.763-0.565-0.742-1.784-1.113-3.657-1.113l-5.883-0.053-4.346 21.783-7.314 0 7.632-38.054 0 0'/%3E%3Cpath d='m70.412 10.123 14.204 0c4.169 0.035 7.19 1.237 9.063 3.604 1.873 2.367 2.491 5.6 1.855 9.699-0.247 1.873-0.795 3.71-1.643 5.512-0.813 1.802-1.943 3.427-3.392 4.876-1.767 1.837-3.657 3.003-5.671 3.498-2.014 0.495-4.099 0.742-6.254 0.742l-6.36 0-2.014 10.07-7.367 0 7.579-38.001 0 0m6.201 6.042-3.18 15.9c0.212 0.035 0.424 0.053 0.636 0.053 0.247 0 0.495 0 0.742 0 3.392 0.035 6.219-0.3 8.48-1.007 2.261-0.742 3.781-3.321 4.558-7.738 0.636-3.71 0-5.848-1.908-6.413-1.873-0.565-4.222-0.83-7.049-0.795-0.424 0.035-0.83 0.053-1.219 0.053-0.353 0-0.724 0-1.113 0l0.053-0.053'/%3E%3C/svg%3E%0A">
 
-    <meta name="description" content="View your phpinfo() output in a pretty, responsive interface">
+    <meta name="description" content="View your phpinfo() output in a pretty, responsive, searchable interface">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <style>
@@ -35,7 +35,7 @@
             <div class="relative group">
                 <input type="search" class="w-48 md:w-72 lg:w-96 bg-gray-100 rounded-full px-4 py-2 text-sm md:text-base border border-gray-100 focus:outline-0 focus:bg-white focus:border-gray-400"
                        placeholder="Type to search..." x-model.debounce="search" x-ref="search" @keydown.stop="" x-on:focus="searchFocused = true" x-on:blur="searchFocused = false"/>
-                <div x-show="!searchFocused && !filtered()" class="absolute top-0 right-0 mt-2 mr-4 bg-white rounded px-2 py-1 text-xs text-gray-500">/</div>
+                <div x-show="!searchFocused && isUnfiltered()" class="absolute top-0 right-0 mt-2 mr-4 bg-white rounded px-2 py-1 text-xs text-gray-500">/</div>
             </div>
         </div>
         <div class="flex-1 flex justify-end items-center gap-4 text-gray-400">
@@ -51,7 +51,7 @@
     </header>
 
     <div class="fixed w-full top-16 lg:top-20 bottom-0 overflow-y-auto bg-gray-100">
-        <div x-show="filteredModules().length == 0" class="max-w-3xl mx-auto mt-20 flex gap-4 justify-center text-gray-500 text-xl">
+        <div x-show="emptyState" class="max-w-3xl mx-auto mt-20 flex gap-4 justify-center text-gray-500 text-xl">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
               <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>
@@ -61,10 +61,11 @@
 
         <div class="flex-1 flex max-w-[96rem] mx-auto">
             <aside class="fixed top-16 lg:top-20 bottom-0 overflow-y-auto hidden md:block flex-shrink-0 w-48 lg:w-56 xl:w-64 py-8 px-4 xl:px-8 space-y-px scroll-py-8">
-                <template x-for="module in filteredModules()" :key="module.key">
-                    <a :id="'nav_' + module.key" @click=jump(module.key) :href="'#' + module.key" class="px-4 py-1 rounded block"
-                       :class="selected == module.key ? 'bg-gray-200' : 'hover:bg-white'"
-                       @click="selectModule(module.key)">
+                <template x-for="module in info.modules" :key="module.key">
+                    <a x-show="module.shouldShow"
+                       :id="'nav_' + module.key"
+                       :href="'#' + module.key" @click=jump(module.key)
+                       class="px-4 py-1 rounded block" :class="selected == module.key ? 'bg-gray-200' : 'hover:bg-white'">
                         <span x-text="module.name"></span>
                     </a>
                 </template>
@@ -72,9 +73,10 @@
 
             <article class="flex-1 md:ml-52 lg:ml-60 xl:ml-72 py-8">
                 <div class="md:px-4 md:pl-0 xl:pr-8">
-                    <template x-for="module in filteredModules()" :key="module.key">
+                    <template x-for="module in info.modules" :key="module.key">
                         <section x-intersect:enter.margin.-100px="enter(module.key)"
                                  x-intersect:leave.margin.-100px="leave(module.key)"
+                                 x-show="module.shouldShow"
                                  class="md:space-y-4 lg:space-y-8 md:mb-4 lg:mb-8 md:scroll-mt-8" :id="module.key">
                             <h2 class="block text-xl font-bold text-gray-900 pl-6 md:pl-0 py-2 md:py-0 sticky md:relative top-0 bg-gray-100 border-b border-gray-200 md:border-0 z-20">
                                 <a :href="'#' + module.key" @click="jump(module.key)" class="group inline-flex items-center gap-2">
@@ -86,7 +88,7 @@
                             </h2>
 
                             <template x-for="(group, index) in module.groups" :key="'group' + index">
-                                <div x-show="group.configs.length" class="table-wrapper md:shadow md:rounded-md bg-white overflow-hidden">
+                                <div x-show="group.shouldShow" class="table-wrapper md:shadow md:rounded-md bg-white overflow-hidden">
                                     <table class="w-full text-sm">
                                         <thead>
                                             <tr x-show="group && group.hasHeadings" class="hidden lg:table-row bg-gray-200 text-gray-900 font-semibold">
@@ -99,6 +101,7 @@
                                         <tbody class="">
                                             <template x-for="config in group.configs" :key="config.key">
                                                 <tr class="flex flex-col py-2 lg:py-0 lg:table-row border-b border-gray-200"
+                                                    x-show="config.shouldShow"
                                                     :class="hash == config.key && 'bg-yellow-100'">
                                                     <td class="lg:w-1/4 flex-shrink-0 align-top py-2 lg:py-4 pl-6 lg:pl-4 font-semibold text-gray-500">
                                                         <a :id="config.key" :href="'#' + config.key"
@@ -136,10 +139,14 @@
     <div x-cloak x-transition.opacity x-show="mobileNav" class="fixed inset-0 overflow-hidden bg-gray-900/50 backdrop-blur-sm z-20">
         <div x-show="mobileNav" @click.away="hideMobileNav()" class="fixed top-0 bottom-0 right-0 w-80 bg-gray-800 z-30 ">
             <nav class="absolute inset-0 overflow-y-auto p-6 pt-16 space-y-px text-white">
-                <template x-for="module in filteredModules()" :key="module.key">
-                    <a :id="'mobile_nav_' + module.key" :href="'#' + module.key" @click="hideMobileNav()" class="px-4 py-1 rounded block"
+                <template x-for="module in info.modules" :key="module.key">
+                    <a x-show="module.shouldShow"
+                       :id="'mobile_nav_' + module.key"
+                       :href="'#' + module.key" @click="hideMobileNav()"
+                       class="px-4 py-1 rounded block"
                        :class="selected == module.key ? 'bg-gray-600' : ''"
-                       @click="selectModule(module.key')" x-text="module.name"></a>
+                       @click="selectModule(module.key')"
+                       x-text="module.name"></a>
                 </template>
             </nav>
 
@@ -158,12 +165,14 @@
         Alpine.data('Navigation', () => ({
             hash: null,
             mobileNav: false,
-            modules: <?php echo json_encode($info->modules()->map->key()->values()) ?>,
+            info: <?php echo json_encode($info) ?>,
+            sections: <?php echo json_encode($info->modules()->map->key()->values()) ?>,
             selected: null,
             selectedIndex: null,
             initialized: false,
             search: null,
             searchFocused: false,
+            emptyState: false,
             init() {
                 if(window.location.hash != '') this.hash = window.location.hash.replace("#","");
                 window.addEventListener('hashchange', () => this.hash = window.location.hash.replace("#",""), false);
@@ -179,31 +188,49 @@
                     document.querySelector(`#nav_${this.selected}`).scrollIntoView({block: "center"});
                     this.initialized = true;
                 });
-            },
-            info() {
-                return <?php echo json_encode($info) ?>;
-            },
-            filteredModules() {
-                if(this.unfiltered()) return this.info().modules;
 
-                return this.info().modules.filter((module) => {
-                    module.groups = module.groups.filter((group) => {
-                        group.configs = group.configs.filter((config) => {
-                            return config.name.toLowerCase().includes(this.search.toLowerCase())
-                                || config.localValue.toLowerCase().includes(this.search.toLowerCase())
-                                || (config.hasMasterValue && config.masterValue.toLowerCase().includes(this.search.toLowerCase()))
-                        });
+                this.$watch('search', () => {
+                    if(this.isFiltered()) {
+                        this.applyVisibleFlags();
+                    } else {
+                        this.allVisible();
+                    }
+                });
 
-                        return group.configs.length > 0;
+                this.allVisible();
+            },
+            allVisible() {
+                this.info.modules.forEach((module) => {
+                    module.groups.forEach((group) => {
+                        group.configs.forEach((config) => config.shouldShow = true);
+                        group.shouldShow = true;
                     });
 
-                    return module.groups.length > 0;
+                    module.shouldShow = true;
                 });
+                this.emptyState = false;
             },
-            filtered() {
-                return !this.unfiltered();
+            applyVisibleFlags() {
+                this.info.modules.forEach((module) => {
+                    module.groups.forEach((group) => {
+                        group.configs.forEach((config) => {
+                            config.shouldShow = config.name.toLowerCase().includes(this.search.toLowerCase())
+                                || config.localValue.toLowerCase().includes(this.search.toLowerCase())
+                                || (config.hasMasterValue && config.masterValue.toLowerCase().includes(this.search.toLowerCase()));
+                        });
+
+                        group.shouldShow = group.configs.filter((config) => config.shouldShow).length > 0;
+                    });
+
+                    module.shouldShow = module.groups.filter((group) => group.shouldShow).length > 0;
+                });
+
+                this.emptyState = this.info.modules.filter((module) => module.shouldShow).length === 0;
             },
-            unfiltered() {
+            isFiltered() {
+                return !this.isUnfiltered();
+            },
+            isUnfiltered() {
                 return this.search == null || this.search == '';
             },
             firstModuleVisible() {
@@ -214,14 +241,14 @@
                 return first ? first.id : null;
             },
             enter(key) {
-                let index = this.modules.indexOf(key);
+                let index = this.sections.indexOf(key);
 
                 if (this.initialized && (this.selectedIndex == null || index < this.selectedIndex || this.selectedNoLongerVisible())) {
                     this.select(index);
                 }
             },
             leave(key) {
-                let index = this.modules.indexOf(key);
+                let index = this.sections.indexOf(key);
 
                 if (this.initialized && (this.selectedIndex == null || this.selectedIndex == index || this.selectedNoLongerVisible())) {
                     this.selectNextIndex();
@@ -231,22 +258,22 @@
                 this.selectModule(key);
             },
             isModule(key) {
-                return this.modules.indexOf(key) > -1;
+                return this.sections.indexOf(key) > -1;
             },
             select(index) {
-                if(this.modules[index] === undefined) return;
+                if(this.sections[index] === undefined) return;
 
                 this.selectedIndex = index;
-                this.selected = this.modules[index];
+                this.selected = this.sections[index];
                 this.scrollIntoView();
             },
             selectNextIndex() {
-                if(this.unfiltered()) return this.select(this.selectedIndex + 1);
+                if(this.isUnfiltered()) return this.select(this.selectedIndex + 1);
 
                 this.selectModule(this.firstModuleVisible());
             },
             selectModule(key) {
-                if(this.isModule(key)) this.select(this.modules.indexOf(key));
+                if(this.isModule(key)) this.select(this.sections.indexOf(key));
             },
             selectedNoLongerVisible() {
                 let el = document.querySelector("#" + this.selected);
@@ -268,7 +295,7 @@
                 this.mobileNav = false;
             },
             highlighted(text) {
-                if(this.unfiltered()) return text;
+                if(this.isUnfiltered()) return text;
 
                 if(text == null) return null;
 
