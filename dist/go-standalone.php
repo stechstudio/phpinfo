b62ee14ddc221058e17e29e3876cdb6791d48409
Joseph Szobody <joseph@stechstudio.com>
2026-03-19 20:35:53 -0400

    Fix Windows path separator and start command quoting

--- dist/go-standalone.php
+++ dist/go-standalone.php
@@ -684,13 +684,13 @@ if (!stream_isatty(STDOUT)) {
     exit;
 }
 
-$file = getcwd() . '/phpinfo.html';
+$file = getcwd() . DIRECTORY_SEPARATOR . 'phpinfo.html';
 file_put_contents($file, $html);
 
 $opened = match (PHP_OS_FAMILY) {
     'Darwin' => !exec('open ' . escapeshellarg($file)),
     'Linux' => !exec('xdg-open ' . escapeshellarg($file) . ' 2>/dev/null &'),
-    'Windows' => !exec('start "" ' . escapeshellarg($file)),
+    'Windows' => !exec('start "" "' . addcslashes($file, '"') . '"'),
     default => false,
 };
 
